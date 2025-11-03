a222dc6db1c66e5c659d63235eef7c0c68989a2f
Manuel Carbajal <manuelcarbajalleon@gmail.com>
2025-11-03 11:39:48 -0500

    Add files via upload

---
+++ ARMS-LS.Logic/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ARMS_LS.Entities;
+
+namespace ARMS_LS.Logic
+{
+    public class Customer
+    {
+        //public static DataTable customerList(string strCustomer)
+        //{ 
+            //return DataAccess.Customer.customerList(strCustomer);
+        //}
+
+    }
+}
